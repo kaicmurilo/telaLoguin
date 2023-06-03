ce393f14351c7f787feaa6121a7e14c3f78a3bc3
Kaic Nunes <kaicmurilo@live.com>
2023-06-02 20:10:39 -0400

    removendo lixo

--- model.py
+++
@@ -1,9 +0,0 @@
-class UploadModel:
-    def __init__(self):
-        self.file_path = ''
-
-    def set_file_path(self, file_path):
-        self.file_path = file_path
-
-    def get_file_path(self):
-        return self.file_path
